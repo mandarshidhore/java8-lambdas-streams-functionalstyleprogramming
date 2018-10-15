9216e33953845d2f9166f80f5e752a66fc63a072
Mandar.Shidhore <mandar.learnstuff@gmail.com>
2018-10-14 21:48:19 -0600

    flatmap example

---
+++ src/main/java/flatmap/FlatMap1.java
@@ -0,0 +1,24 @@
+package flatmap;
+
+import java.util.Arrays;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+// Challenge ->
+// 		Given two lists of numbers, return all pairs of numbers.
+//		e.g. given a list [1, 2, 3] and a list [3, 4], return [(1,3), (1,4), (2,3), (2,4), (3,3), (3,4)]
+
+public class FlatMap1 {
+
+	public static void main(String[] args) {
+		List<Integer> firstList = Arrays.asList(1, 2, 3);
+		List<Integer> secondList = Arrays.asList(3, 4);
+
+		List<int[]> pairs = firstList.stream().flatMap(i -> secondList.stream().map(j -> new int[] { i, j }))
+				.collect(toList());
+
+		// print to console
+		pairs.stream().forEach(i -> System.out.println(Arrays.toString(i)));
+	}
+
+}
