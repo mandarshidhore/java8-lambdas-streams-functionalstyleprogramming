4f78b778e607739da9ffecf53851d5ad292fbcec
mandarshidhore <mandar.learnstuff@gmail.com>
2018-10-25 20:33:12 -0600

    more stuff

--- src/main/java/com/sssm/lambdasandstreams/filter/Filter2.java
+++ src/main/java/com/sssm/lambdasandstreams/filter/Filter2.java
@@ -1,6 +1,7 @@
 package com.sssm.lambdasandstreams.filter;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 // Filter & Truncate
@@ -15,20 +16,26 @@ public class Filter2 {
 
 		// Find first four unique even numbers
 		System.out.println("\nFirst four unique even numbers unsorted = ");
-		// this line should print 4 followed by 2 followed by 6 since it is unsorted
+		// following should print 4 then 2 then 6 since it is unsorted
 		numbers.stream().filter(i -> i % 2 == 0).distinct().limit(4).forEach(System.out::println);
-		
+
 		System.out.println("\nFirst two unique even numbers unsorted = ");
-		// this line should print 4 followed by 2 since it is unsorted
+		// this line should print 4 then 2 since it is unsorted
 		numbers.stream().filter(i -> i % 2 == 0).distinct().limit(2).forEach(System.out::println);
 
-		// sort the list
-		numbers.sort((i1, i2) -> i1.compareTo(i2));
-		
 		// if source is sorted, result will be sorted.
 		System.out.println("\nFirst two unique even numbers sorted = ");
-		// this line should print 2 followed by 4 since it is sorted
-		numbers.stream().filter(i -> i % 2 == 0).distinct().limit(2).forEach(System.out::println);
+		// this line should print 2 then 4 since it is sorted
+		numbers.stream().filter(i -> i % 2 == 0).distinct().sorted().limit(2).forEach(System.out::println);
+
+		// a given list can also be sorted as below
+		// numbers.sort((i1, i2) -> i1.compareTo(i2));
+
+		// if source is sorted, result will be sorted.
+		System.out.println("\nFirst three unique even numbers sorted in reverse order = ");
+		// following should print 6 then 4 then 2 since it is sorted in reverse order
+		numbers.stream().filter(i -> i % 2 == 0).distinct().sorted(Comparator.reverseOrder()).limit(3)
+				.forEach(System.out::println);
 	}
 
 }
